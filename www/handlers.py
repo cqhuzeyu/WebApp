f540b58c891eab62cf23e898d82d0bcee78c3b76
cqhuzeyu <742918498@qq.com>
2015-10-12 17:32:45 +0800

    big step

--- www/handlers.py
+++ www/handlers.py
@@ -10,8 +10,21 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 def index(request):
-    users = yield from User.findAll()
+    summary = 'Whenever you cry, there must be some elves swimming in your heart ocean, so dim, so quiet.'
+    blogs = [
+            Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+            Blog(id='2', name='Something weird', summary=summary, created_at=time.time()-3600),
+            Blog(id='3', name='Learn Python', summary=summary, created_at=time.time()-7200)
+            ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
+
+
+@get('/api/users')
+def api_get_users(*, page='1'):
+    users = yield from User.findAll(orderBy='created_at desc') 
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
