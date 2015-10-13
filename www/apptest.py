c3be86613ed15c6b56c97c2e5c0a056d5433cb9d
cqhuzeyu <742918498@qq.com>
2015-10-13 17:30:51 +0800

    another big step

--- www/apptest.py
+++ www/apptest.py
@@ -6,11 +6,11 @@ from models import User, Blog, Comment
 @asyncio.coroutine
 def test(loop):
 	yield from orm.create_pool(loop = loop, host='127.0.0.1', port=3306, user='root', password='19900411', db='awesome')
-	#u = User(name='Test', email='tttest@example.com', passwd='1234567890', image='about:blank')
-	#yield from u.save()
-	l = yield from User.findAll()
-	for x in l:
-	    print(x)
+	u = User(name='zeyuhu', email='zeyuhu@example.com', passwd='19900411', image='about:blank', admin=1)
+	yield from u.save()
+	#l = yield from User.findAll()
+	#for x in l:
+	#    print(x)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(test(loop))
